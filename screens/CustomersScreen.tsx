994c6efc1be85efdd0dd69fd4400f28efd714326
Shadman Sajid <88870507+s-sajid@users.noreply.github.com>
2022-09-16 00:22:07 -0600

    Add Customer Screen Header Image

--- screens/CustomersScreen.tsx
+++ screens/CustomersScreen.tsx
@@ -1,14 +1,36 @@
-import { Text, SafeAreaView } from 'react-native';
-import React from 'react';
+import { ScrollView } from 'react-native';
+import React, { useLayoutEffect } from 'react';
 import { useTailwind } from 'tailwind-rn/dist';
+import { CompositeNavigationProp, useNavigation } from '@react-navigation/native';
+import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
+import { TabStackParamList } from '../navigator/TabNavigator';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { RootStackParamList } from '../navigator/RootNavigator';
+import { Image } from '@rneui/themed';
+
+export type CustomerScreenNaviagtionProp = CompositeNavigationProp<
+  BottomTabNavigationProp<TabStackParamList, 'Customers'>,
+  NativeStackNavigationProp<RootStackParamList>
+>;
 
 const CustomersScreen = () => {
     const tw = useTailwind();
+    const navigation = useNavigation<CustomerScreenNaviagtionProp>();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    })
+  }, [])
+
 
   return (
-    <SafeAreaView>
-      <Text style={tw("text-blue-500")} >CustomersScreen</Text>
-    </SafeAreaView>
+    <ScrollView>
+      <Image         
+        source={require('../images/CustomersScreenHeader.jpeg')} 
+        style= {{ width: 400, height: 250 }}
+      />
+    </ScrollView>
   );
 }
 
