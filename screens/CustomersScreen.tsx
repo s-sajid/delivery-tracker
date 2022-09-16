e0b91db7ce43e486e250506c645acd734bc09517
Shadman Sajid <88870507+s-sajid@users.noreply.github.com>
2022-09-16 00:35:20 -0600

    Add Customer Screen Search Bar

--- screens/CustomersScreen.tsx
+++ screens/CustomersScreen.tsx
@@ -1,12 +1,12 @@
 import { ScrollView } from 'react-native';
-import React, { useLayoutEffect } from 'react';
+import React, { useLayoutEffect, useState } from 'react';
 import { useTailwind } from 'tailwind-rn/dist';
 import { CompositeNavigationProp, useNavigation } from '@react-navigation/native';
 import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
 import { TabStackParamList } from '../navigator/TabNavigator';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../navigator/RootNavigator';
-import { Image } from '@rneui/themed';
+import { Image, Input } from '@rneui/themed';
 
 export type CustomerScreenNaviagtionProp = CompositeNavigationProp<
   BottomTabNavigationProp<TabStackParamList, 'Customers'>,
@@ -16,19 +16,26 @@ export type CustomerScreenNaviagtionProp = CompositeNavigationProp<
 const CustomersScreen = () => {
     const tw = useTailwind();
     const navigation = useNavigation<CustomerScreenNaviagtionProp>();
+    const [input, setInput] = useState<string>('');
 
   useLayoutEffect(() => {
     navigation.setOptions({
       headerShown: false,
-    })
-  }, [])
-
+    });
+  }, []);
 
   return (
-    <ScrollView>
+    <ScrollView style= {{ backgroundColor: '#59C1CC' }} >
       <Image         
-        source={require('../images/CustomersScreenHeader.jpeg')} 
+        source= {require('../images/CustomersScreenHeader.jpeg')} 
         style= {{ width: 400, height: 250 }}
+      />
+
+      <Input 
+        placeholder='Search by Customer' 
+        value= {input} 
+        onChangeText= {setInput}  
+        containerStyle= {tw('bg-white pt-5 pb-0 px-10')}
       />
     </ScrollView>
   );
