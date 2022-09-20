609e22ba2ee6fc185d8b1cc523ea9886eab8ef14
Shadman Sajid <88870507+s-sajid@users.noreply.github.com>
2022-09-20 01:19:42 -0600

    Add Apollo Client

--- App.tsx
+++ App.tsx
@@ -3,14 +3,22 @@ import {TailwindProvider} from 'tailwind-rn';
 import utilities from './tailwind.json';
 import { NavigationContainer } from '@react-navigation/native';
 import RootNavigator from './navigator/RootNavigator';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5001/api/delivery-tracker',
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   return (
     // @ts-ignore TailwindProvider
     <TailwindProvider utilities={utilities}>
-      <NavigationContainer>
-        <RootNavigator />
-      </NavigationContainer>
+      <ApolloProvider client={client}>
+        <NavigationContainer>
+          <RootNavigator />
+        </NavigationContainer>
+      </ApolloProvider>
     </TailwindProvider>
   );
 }
