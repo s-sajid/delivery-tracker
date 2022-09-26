9aa9000c0e252b83ab1951cf1ec57176184c473b
Shadman Sajid <88870507+s-sajid@users.noreply.github.com>
2022-09-26 09:20:20 -0600

    Add React Native Maps to Delivery Card

--- components/DeliveryCard.tsx
+++ components/DeliveryCard.tsx
@@ -2,6 +2,7 @@ import { View, Text } from "react-native";
 import React from "react";
 import { useTailwind } from "tailwind-rn/dist";
 import { Card, Divider, Icon } from "@rneui/themed";
+import MapView, { Marker } from "react-native-maps";
 
 type Props = {
   order: Order;
@@ -40,7 +41,7 @@ const DeliveryCard = ({ order }: Props) => {
           <Divider color="white" />
         </View>
 
-        <View style={tw("mx-auto")}>
+        <View style={tw("mx-auto pb-5")}>
           <Text style={tw(" text-base text-center text-white font-bold mt-5")}>
             Address
           </Text>
@@ -66,6 +67,28 @@ const DeliveryCard = ({ order }: Props) => {
           </View>
         ))}
       </View>
+
+      <MapView
+        style={[tw("w-full"), { height: 200 }]}
+        initialRegion={{
+          latitude: order.Lat,
+          longitude: order.Lng,
+          latitudeDelta: 0.005,
+          longitudeDelta: 0.005,
+        }}
+      >
+        {order.Lat && order.Lng && (
+          <Marker
+            coordinate={{
+              latitude: order.Lat,
+              longitude: order.Lng,
+            }}
+            title="Delivery Location"
+            description={order.Address}
+            identifier="origin"
+          />
+        )}
+      </MapView>
     </Card>
   );
 };
