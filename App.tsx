c878a6b49fb7922943bacdc4d3db37d347b2aaf1
Shadman Sajid <88870507+s-sajid@users.noreply.github.com>
2022-09-26 09:03:26 -0600

    Add Delivery Card Component

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
-import { StyleSheet } from 'react-native';
-import {TailwindProvider} from 'tailwind-rn';
-import utilities from './tailwind.json';
-import { NavigationContainer } from '@react-navigation/native';
-import RootNavigator from './navigator/RootNavigator';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
-import apolloUri from './config/env';
+import { StyleSheet } from "react-native";
+import { TailwindProvider } from "tailwind-rn";
+import utilities from "./tailwind.json";
+import { NavigationContainer } from "@react-navigation/native";
+import RootNavigator from "./navigator/RootNavigator";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import apolloUri from "./config/env";
 
 const client = new ApolloClient({
   uri: `${apolloUri}`,
@@ -27,8 +27,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
