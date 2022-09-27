b6373caa3733f4938d5454aca027b00ddb091124
Shadman Sajid <88870507+s-sajid@users.noreply.github.com>
2022-09-27 16:57:40 -0600

    Add Image to Orders Screen

---
+++ components/OrderCard.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const OrderCard = () => {
+  return (
+    <View>
+      <Text>OrderCard</Text>
+    </View>
+  );
+};
+
+export default OrderCard;
